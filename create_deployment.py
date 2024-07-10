fc8ee2ec20c767da46847bf9c32916b5e8430c1a
danjsmith1510 <danieljsmith1510@gmail.com>
2024-07-10 22:03:21 +1000

    added requirements.txt

--- create_deployment.py
+++ create_deployment.py
@@ -2,10 +2,10 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/prefecthq/demos.git",
-        entrypoint="pocketsmith.py:get_pocketsmith_data",
+        source="https://github.com/danjsmith1510/prefect-finances.git",
+        entrypoint="finance_workflow.py:finance_workflow",
     ).deploy(
-        name="get-pocketsmith-data",
+        name="finance_workflow",
         work_pool_name="my-managed-pool",
         cron="0 1 * * *",
     )
