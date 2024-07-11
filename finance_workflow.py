0b6166b602baed2c41a60a23e6435f57c97dbf9b
danjsmith1510 <danieljsmith1510@gmail.com>
2024-07-11 15:30:50 +1000

    Update finance_workflow.py

--- finance_workflow.py
+++ finance_workflow.py
@@ -14,24 +14,24 @@ def finance_workflow():
 
     print ('TEST RUN')
 
-    # accounts = get_pocketsmith_accounts()
-    # print(f"Got {len(accounts)} accounts")
-    # load_extract_table('accounts', json.dumps(accounts))
+    accounts = get_pocketsmith_accounts()
+    print(f"Got {len(accounts)} accounts")
+    load_extract_table('accounts', json.dumps(accounts))
 
-    # categories = get_pocketsmith_categories()
-    # print(f"Got {len(categories)} categories")
-    # load_extract_table('category', json.dumps(categories))
+    categories = get_pocketsmith_categories()
+    print(f"Got {len(categories)} categories")
+    load_extract_table('category', json.dumps(categories))
 
-    # transactions = get_pocketsmith_transactions(start_date, end_date)
-    # print(f"Got {len(transactions)} transactions")
-    # load_extract_table('transaction', json.dumps(transactions))
+    transactions = get_pocketsmith_transactions(start_date, end_date)
+    print(f"Got {len(transactions)} transactions")
+    load_extract_table('transaction', json.dumps(transactions))
 
-    # tasks = get_jira_stories()
-    # print(f"Got {len(tasks)} tasks")
-    # load_extract_table('task', json.dumps(tasks))
+    tasks = get_jira_stories()
+    print(f"Got {len(tasks)} tasks")
+    load_extract_table('task', json.dumps(tasks))
 
-    # run_merge = run_merge_extracts(start_date, end_date)
-    # print (run_merge)
+    run_merge = run_merge_extracts(start_date, end_date)
+    print (run_merge)
 
 if __name__ == "__main__":
     finance_workflow()
