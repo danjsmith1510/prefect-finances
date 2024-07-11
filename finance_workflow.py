9d87f46c8bc26b663624184b630fef4ff2d11db5
danjsmith1510 <danieljsmith1510@gmail.com>
2024-07-11 15:31:09 +1000

    Update finance_workflow.py

--- finance_workflow.py
+++ finance_workflow.py
@@ -1,13 +1,13 @@
-# import json
+import json
 from prefect import flow
-# import datetime as dt
-# from tasks.finance_database import load_extract_table, run_merge_extracts
-# from tasks.pocketsmith import get_pocketsmith_accounts, get_pocketsmith_categories, get_pocketsmith_transactions
-# from tasks.jira import get_jira_stories
+import datetime as dt
+from tasks.finance_database import load_extract_table, run_merge_extracts
+from tasks.pocketsmith import get_pocketsmith_accounts, get_pocketsmith_categories, get_pocketsmith_transactions
+from tasks.jira import get_jira_stories
 
-# start_date_raw = dt.datetime.today() - dt.timedelta(days=7)
-# start_date = start_date_raw.strftime('%Y-%m-%d')
-# end_date = dt.datetime.today().strftime('%Y-%m-%d')
+start_date_raw = dt.datetime.today() - dt.timedelta(days=7)
+start_date = start_date_raw.strftime('%Y-%m-%d')
+end_date = dt.datetime.today().strftime('%Y-%m-%d')
 
 @flow(log_prints=True)
 def finance_workflow():
